91fdbeb47f79269451aa2eac9bbff7d4e6fae93a
ale18V <alebombarda@icloud.com>
2024-12-13 01:23:16 +0100

    Configure logging to be a library

--- packages/blockchain/src/blockchain/__init__.py
+++ packages/blockchain/src/blockchain/__init__.py
@@ -3,14 +3,24 @@ import sys
 from loguru import logger
 
 
-_all_ = ["models", "node"]
+_all_ = ["models", "node", "configure_logging"]
 
-logger.remove()
-level = "DEBUG" if os.getenv("DEBUG", False) else "INFO"
-logger.add(
-    sys.stdout,
-    format="<level>{time:HH:mm:ss.SSS} {level}</level> <cyan>{name}:{function}:{line}</cyan>: {message}",
-    filter=lambda record: record["extra"].get("emitter") != "Tendermint",
-    level=level,
-)
-logger.level("INFO", color="<green>")
+logger.disable("blockchain")
+
+
+def configure_logging() -> None:
+    logger.enable("blockchain")
+    logger.remove()
+    level = "DEBUG" if os.getenv("DEBUG", False) else "INFO"
+    logger.add(
+        sys.stdout,
+        format="<level>{time:HH:mm:ss.SSS} {level}</level> <cyan>{file}:{function}:{line}</cyan>: {message}",
+        filter=lambda record: record["extra"].get("emitter") != "Tendermint",
+        level=level,
+    )
+    logger.level("INFO", color="<green>")
+    logger.disable("blockchain.services._internal.network")
+    logger.disable("blockchain.server")
+
+
+configure_logging()
